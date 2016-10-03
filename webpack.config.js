0e7db116ffcde7b0d97abea454d9cd13da6dbaca
Michał Jezierski <ambinanitelo@gmail.com>
2016-10-03 22:07:55 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        bundle: './src/index.js'
+    },
+    output: {
+        path: path.join(__dirname, 'target'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        publicPath: 'target'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }]
+    }
+};
